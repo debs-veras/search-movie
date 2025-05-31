054ef1a87db2a46aa5e0371ab80b18bf323a7a4a
Debora Veras <deboraveras.inf1.ep@outlook.com>
2025-05-31 15:57:17 -0300

    feat: mudancas de layout e teste na api da TMDB

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,4 @@
 export const API_KEY = "60bcd903";
 export const API_URL = "https://www.omdbapi.com";
+export const API_URL_TMDB = "https://api.themoviedb.org/3";
+export const API_kEY_TMDB = "c82e5a4a26d33a1e3ca752a5daa59d54";
--- src/services/searchMovie.ts
+++ src/services/searchMovie.ts
@@ -1,5 +1,23 @@
+import axios from "axios";
+import { API_kEY_TMDB, API_URL_TMDB } from "../constants/api";
 import { getRequest } from "../utils/axiosRequest";
 
 export const getBuscaFilme = async (url: string) => {
   return await getRequest(`?${url}`);
+};
+
+
+export const getBuscaFilmeTmdb = async (query: string, type = "movie") => {
+  const endpoint = type === "tv" ? "search/tv" : "search/movie";
+
+  const response = await axios.get(`${API_URL_TMDB}/${endpoint}`, {
+    params: {
+      api_key: API_kEY_TMDB,
+      language: "pt-BR",
+      query,
+      include_adult: false,
+    },
+  });
+
+  return response.data.results;
 };
